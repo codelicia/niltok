1c4b6306c37723a863229f14f4d567f9a320e864
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-15 14:29:56 +0100

    feat: implements windowed() function
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

---
+++ src/Iterable/windowed.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelicia\Niltok\Iterable;
+
+use ArrayObject;
+use Codelicia\Niltok\Ranges\IntRange;
+
+use function Codelicia\Niltok\Standard\entails;
+
+/**
+ * Returns a list of snapshots of the window of the given {@see $size}
+ * sliding along this collection with the given {@see $step}, where each
+ * snapshot is a list.
+ *
+ * Several last lists may have fewer elements than the given {@see $size}.
+ *
+ * Both {@see $size} and {@see $step} must be positive and can be greater than the number of elements in this collection.
+ *
+ * @param array $iterable Collection to apply the windows
+ * @param int $size The number of elements to take in each window
+ * @param int $step The number of elements to move the window forward by on each step, by default 1
+ */
+function windowed(
+    iterable $iterable,
+    int $size,
+    int $step = 1,
+    ?callable $transform = null
+): ArrayObject {
+    entails($size > 0 && $step > 0, "Both size $size and step $step must be greater than zero.");
+
+    $transform = $transform ?? (static fn ($x) => $x);
+
+    $thisSize = count($iterable);
+    $result = new ArrayObject();
+    $index = 0;
+    $range = new IntRange(0, $thisSize - 1);
+    while ($range->contains($index)) {
+        $windowSize = min($size, ($thisSize - $index));
+        if ($size > $windowSize) {
+            break;
+        }
+        $result[] = $transform(array_slice($iterable, $index, $windowSize));
+        $index += $step;
+    }
+
+    return $result;
+}
---
+++ tests/functional/windowed.phpt
@@ -0,0 +1,28 @@
+--TEST--
+Test the windowed function.
+--FILE--
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Codelicia\Niltok\Ranges\IntRange;
+use function Codelicia\Niltok\Iterable\windowed;
+
+$windowed = windowed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], size: 2, step: 1);
+
+printf("[%s]\n", implode(', ', array_map(fn (array $window) => "[$window[0], $window[1]]", $windowed->getArrayCopy())));
+
+$windowed = windowed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], size: 3, step: 3);
+
+printf("[%s]\n", implode(', ', array_map(fn (array $window) => "[$window[0], $window[1], $window[2]]", $windowed->getArrayCopy())));
+
+$windowed = windowed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], size: 3, step: 3, transform: fn (array $x) => count($x));
+
+printf('[%s]', implode(', ', $windowed->getArrayCopy()));
+
+--EXPECTF--
+[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]]
+[[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+[3, 3, 3]

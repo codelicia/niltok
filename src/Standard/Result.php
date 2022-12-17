e659099483d55adfecd2e2d344613f8c51fac45b
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-17 20:04:27 +0100

    feat: first draft of the class API
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

---
+++ src/Standard/Result.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelicia\Niltok\Standard;
+
+use JsonSerializable;
+
+/**
+ * A discriminated union that encapsulates a successful outcome with a value of type [T]
+ * or a failure with an arbitrary [Throwable] exception.
+ *
+ * @param-out T
+ */
+final class Result implements JsonSerializable
+{
+    public function __construct(private mixed $value)
+    {
+    }
+
+    public function isSuccess(): bool {
+        TODO();
+    }
+
+    public function isFailure(): bool {
+        TODO();
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        TODO();
+    }
+}
